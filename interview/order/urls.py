8eddad667feef617f0b44b3361a7edbe9c8de9b9
Rutwick Bhawsar <rutwick.bhawsar@tmtinsights.com>
2023-04-13 15:43:19 +0530

    Challenge 3 - Embargo Date - Completed
    Added a view that filters Orders based on Embargo Dates. Also, added a separate url.

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView,\
+    OrderFilterDateView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path('date/<str:start_date>&<str:embargo_date>',OrderFilterDateView.as_view(), name = 'date-range-order')
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.request import Request
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
+
+from datetime import datetime
 
 # Create your views here.
 class OrderListCreateView(generics.ListCreateAPIView):
@@ -13,3 +17,22 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+class OrderFilterDateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        
+        try:
+            start_date_1 = datetime.strptime(kwargs['start_date'],'%Y-%m-%d').date()
+            embargo_date_1 = datetime.strptime(kwargs['embargo_date'],'%Y-%m-%d').date()
+            orders = self.get_queryset(start_date__gte=start_date_1,embargo_date__lte=embargo_date_1)
+            serializer = self.serializer_class(orders,many=True)
+            return Response(serializer.data, status=200)
+        
+        except Exception as e:
+            print("*********EXCEPTION: ",e)
+    
+    def get_queryset(self,**kwargs):
+        return self.queryset.filter(**kwargs)
